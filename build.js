bd408917928dcd7c39951cc0489988bcfd3bd391
sinnybb <2ud2m0nic@gmail.com>
2024-06-15 18:07:37 +0900

    third commit

---
+++ build.js
@@ -0,0 +1,19 @@
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.join(__dirname, 'view'); // view 폴더를 사용
+const distDir = path.join(__dirname, 'dist');
+
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
+fs.readdirSync(srcDir).forEach(file => {
+  if (path.extname(file) === '.ejs') {
+    const ejsContent = fs.readFileSync(path.join(srcDir, file), 'utf8');
+    const htmlContent = ejs.render(ejsContent);
+    const htmlFileName = file.replace('.ejs', '.html');
+    fs.writeFileSync(path.join(distDir, htmlFileName), htmlContent);
+  }
+});
